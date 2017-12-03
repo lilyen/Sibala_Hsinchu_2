3dd2113ceb67cbc634e412ca5ebdba8cdfd4e3c7
Brian <hatedinny1@gmail.com>
2017-12-03 14:49:40 +0800

    Pass Test : DiceComparer_NoPoint_6_1_3_4_Compare_NoPoint_3_4_1_2_Should_be_0

--- Sibala_Hsinchu_2/DiceComparerTest.cs
+++ Sibala_Hsinchu_2/DiceComparerTest.cs
@@ -6,9 +6,19 @@ namespace Sibala_Hsinchu_2
     public class DiceComparerTest
     {
         [TestMethod]
-        public void DiceComparer_NoPoint_Compare_NoPoint_Should_be_0()
+        public void DiceComparer_NoPoint_6_1_3_4_Compare_NoPoint_3_4_1_2_Should_be_0()
         {
-            
+            //arrange
+            var noPoint1 = new Sibara(6, 1, 3, 4);
+            var noPoint2 = new Sibara(3, 4, 1, 2);
+            var diceComparer = new DiceComparer();
+            var expected = 0;
+            //act
+            var actual = diceComparer.Compare(noPoint1, noPoint2);
+            //assert
+            Assert.AreEqual(expected, actual);
         }
+
+
     }
 }
