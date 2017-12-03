63ca03db11d777e8e28b57b389c5182f221bbb32
esawei <esawei5190@gmail.com>
2017-12-03 14:47:03 +0800

    new fail test case: input_1_1_1_1_should_be_sameColor

---
+++ Sibala_Hsinchu_2/SibaraTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sibala_Hsinchu_2
+{
+    [TestClass]
+    public class SibaraTests
+    {
+        [TestMethod]
+        public void input_1_1_1_1_should_be_sameColor()
+        {
+            var target = new Sibara(1, 1, 1, 1);
+            var expected = "same color";
+            var actual = target.SibaraResult;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
