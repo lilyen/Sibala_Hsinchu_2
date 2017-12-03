e1f85839be0bd60cc3adb3638d605b7e06c89759
Brian <hatedinny1@gmail.com>
2017-12-03 15:41:04 +0800

    Pass Test : DiceComparer_SameColor_1_1_1_1_Compare_SameColor_4_4_4_4_Should_Greater_0

--- Sibala_Hsinchu_2/DiceComparer.cs
+++ Sibala_Hsinchu_2/DiceComparer.cs
@@ -8,12 +8,41 @@ namespace Sibala_Hsinchu_2
 {
     class DiceComparer : IComparer<ISibara>
     {
+        private Dictionary<int, int> dict;
+
+        public DiceComparer()
+        {
+            dict = new Dictionary<int, int>()
+            {
+                [0] = 4,
+                [1] = 6,
+                [2] = 10,
+                [3] = 12,
+                [4] = 8,
+                [5] = 2,
+
+            };
+        }
+        
         public int Compare(ISibara firstDice, ISibara secondDice)
         {
+
+            if (firstDice.Status == SibaraStatus.StatusEnum.SameColor &&
+                secondDice.Status == SibaraStatus.StatusEnum.SameColor)
+            {
+                return
+                    dict.First(x => x.Value == firstDice.Points).Key -
+                    dict.First(x => x.Value == secondDice.Points).Key;
+            }
+
             if (firstDice.Status == secondDice.Status)
             {
                 return firstDice.Points - secondDice.Points;
             }
+
+
+
+            
             return firstDice.Status - secondDice.Status;
         }
     }
