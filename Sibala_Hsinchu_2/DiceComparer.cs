c072609bbbc009f9400a2961027a14e97b2bf08b
Brian <hatedinny1@gmail.com>
2017-12-03 14:33:04 +0800

    Initial

--- Sibala_Hsinchu_2/DiceComparer.cs
+++ Sibala_Hsinchu_2/DiceComparer.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Sibala_Hsinchu_2
 {
-    [TestClass]
-    public class DiceComparer
-    {       
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //
-            // TODO:  在此加入測試邏輯
-            //
-        }
+    class DiceComparer
+    {
+
     }
 }
---
+++ Sibala_Hsinchu_2/DiceComparerTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sibala_Hsinchu_2
+{
+    [TestClass]
+    public class DiceComparerTest
+    {
+        [TestMethod]
+        public void DiceComparer_NoPoint_Compare_NoPoint_Should_be_0()
+        {
+            
+        }
+    }
+}
