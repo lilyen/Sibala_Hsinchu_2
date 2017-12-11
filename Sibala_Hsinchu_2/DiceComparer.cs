82adb0b6fba918d7ba06c13a3713bc1854ad6e9d
hatelove <jay.hatelove@gmail.com>
2017-12-11 22:44:06 +0800

    refactor DiceComparer.Compare() algorithm

--- Sibala_Hsinchu_2/DiceComparer.cs
+++ Sibala_Hsinchu_2/DiceComparer.cs
@@ -1,24 +1,18 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace Sibala_Hsinchu_2
 {
     internal class DiceComparer : IComparer<ISibara>
     {
-        private Dictionary<int, int> dict;
-
-        public DiceComparer()
+        private Dictionary<int, int> sameColorCompareWeightLookup = new Dictionary<int, int>
         {
-            dict = new Dictionary<int, int>()
-            {
-                [0] = 4,
-                [1] = 6,
-                [2] = 10,
-                [3] = 12,
-                [4] = 8,
-                [5] = 2,
-            };
-        }
+            {4,1 },
+            {6,2 },
+            {10,3 },
+            {12,4 },
+            {8,5 },
+            {2,6 },
+        };
 
         public int Compare(ISibara firstDice, ISibara secondDice)
         {
@@ -28,21 +22,19 @@ namespace Sibala_Hsinchu_2
                 {
                     return 0;
                 }
-            }
-            if (IsBothDiceSameColor(firstDice, secondDice))
-            {
-                return
-                    dict.First(x => x.Value == firstDice.Points).Key -
-                    dict.First(x => x.Value == secondDice.Points).Key;
-            }
-
-            if (IsBothDiceSameStatus(firstDice, secondDice))
-            {
-                if (firstDice.Points == secondDice.Points)
+                if (firstDice.Status == SibaraStatus.StatusEnum.SameColor)
                 {
-                    return firstDice.MaxPoint - secondDice.MaxPoint;
+                    return sameColorCompareWeightLookup[firstDice.Points] -
+                           sameColorCompareWeightLookup[secondDice.Points];
                 }
-                return firstDice.Points - secondDice.Points;
+                if (firstDice.Status == SibaraStatus.StatusEnum.Point)
+                {
+                    if (firstDice.Points == secondDice.Points)
+                    {
+                        return firstDice.MaxPoint - secondDice.MaxPoint;
+                    }
+                    return firstDice.Points - secondDice.Points;
+                }
             }
 
             return firstDice.Status - secondDice.Status;
