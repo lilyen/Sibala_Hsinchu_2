c9d51b9f4c75941e911194e64c91481eb0f04797
lily.yen79 <lily.yen79@TI03NB61>
2018-01-22 22:17:34 +0800

    change testMethod to testcase

--- Sibala_Hsinchu_2/SibaraTests.cs
+++ Sibala_Hsinchu_2/SibaraTests.cs
@@ -8,54 +8,18 @@ namespace Sibala_Hsinchu_2
     public class SibaraTests
     {
         [TestCase(1, 1, 1, 1, SibaraStatus.StatusEnum.SameColor, "same color", 1, 1, TestName = "input_1_1_1_1_should_be_sameColor")]
+        [TestCase(1, 3, 4, 2, SibaraStatus.StatusEnum.NoPoint, "no points", 0, 0, TestName = "input_1_3_4_2_should_be_noPoints")]
+        [TestCase(3, 6, 3, 3, SibaraStatus.StatusEnum.NoPoint, "no points", 0, 0, TestName = "input_3_6_3_3_should_be_noPoints")]
+        [TestCase(6, 2, 6, 2, SibaraStatus.StatusEnum.Point, "sibala", 12, 6, TestName = "input_6_2_6_2_should_be_Sibala")]
+        [TestCase(5, 5, 2, 2, SibaraStatus.StatusEnum.Point, "10 point", 10, 5, TestName = "input_5_5_2_2_should_be_10_Points")]
+        [TestCase(4, 4, 2, 1, SibaraStatus.StatusEnum.Point, "BG", 3, 2, TestName = "input_4_4_2_1_should_be_BG")]
+        [TestCase(5, 5, 4, 3, SibaraStatus.StatusEnum.Point, "7 point", 7, 4, TestName = "input_5_5_4_3_should_be_7_Points")]
         public void Test_Silbala(int dice1, int dice2, int dice3, int dice4, SibaraStatus.StatusEnum status, string result, int points, int maxPoint)
         {
             var target = new Sibara(dice1, dice2, dice3, dice4);
             VerifySibala(target, status, result, points, maxPoint);
         }
-
-        [TestMethod]
-        public void input_1_3_4_2_should_be_noPoints()
-        {
-            var target = new Sibara(1, 3, 4, 2);
-            VerifySibala(target, SibaraStatus.StatusEnum.NoPoint, "no points", 0, 0);
-        }
-
-        [TestMethod]
-        public void input_3_6_3_3_should_be_noPoints()
-        {
-            var target = new Sibara(3, 6, 3, 3);
-            VerifySibala(target, SibaraStatus.StatusEnum.NoPoint, "no points", 0, 0);
-        }
-
-        [TestMethod]
-        public void input_6_2_6_2_should_be_Sibala()
-        {
-            var target = new Sibara(6, 2, 6, 2);
-            VerifySibala(target, SibaraStatus.StatusEnum.Point, "sibala", 12, 6);
-        }
-
-        [TestMethod]
-        public void input_5_5_2_2_should_be_10_Points()
-        {
-            var target = new Sibara(5, 5, 2, 2);
-            VerifySibala(target, SibaraStatus.StatusEnum.Point, "10 point", 10, 5);
-        }
-
-        [TestMethod]
-        public void input_4_4_2_1_should_be_BG()
-        {
-            var target = new Sibara(4, 4, 2, 1);
-            VerifySibala(target, SibaraStatus.StatusEnum.Point, "BG", 3, 2);
-        }
-
-        [TestMethod]
-        public void input_5_5_4_3_should_be_7_Points()
-        {
-            var target = new Sibara(5, 5, 4, 3);
-            VerifySibala(target, SibaraStatus.StatusEnum.Point, "7 point", 7, 4);
-        }
-
+        
         private void VerifySibala(Sibara target, SibaraStatus.StatusEnum status, string result, int points, int maxPoint)
         {
             Assert.AreEqual(status, target.Status);
